305d7de613fa8ddde7839781d5243fb50a79becb
--add <andrewksmoker@gmail.com>
2021-02-01 12:15:15 -0500

    commit tree 1e0e7c2956445f4a0ecafa580f219561195b3899

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Your code here
+function mapToNegativize(sourceArray) {
+    return sourceArray.map(value => value * -1)
+}
+
+function mapToNoChange(sourceArray) {
+    return sourceArray.map(value => value)
+}
+
+function mapToDouble(sourceArray) {
+    return sourceArray.map(value => value * 2)
+}
+
+function mapToSquare(sourceArray) {
+    return sourceArray.map(value => value*value)
+}
+
+function reduceToTotal(sourceArray, startingPoint=0) {
+    return sourceArray.reduce(
+        (total, value) => total + value, startingPoint 
+    )
+}
+
+function reduceToAllTrue(sourceArray) {
+    return sourceArray.reduce(
+        (state, value) => {
+            if (value === false) return state = false
+            else {return state = true}
+        })
+}
+
+function reduceToAnyTrue(src) {
+    return src.reduce(
+        (state, value) => {
+            if (value === true) return state = true 
+            else {return state = false}
+        }
+
+    )
+}
